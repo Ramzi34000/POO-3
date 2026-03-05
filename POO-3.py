81076e930dad8e4d6d2a926b7a7c7cb09242794a
ramzi <ramzeddine.saadi@gmail.com>
2026-03-05 00:27:47 -0500

    creation final du parc et des voiture

--- POO-3.py
+++ POO-3.py
@@ -21,11 +21,11 @@ class Parc:
 
     def entrerVoiture(self, voiture):
 
-        if voiture in self.listeVoitures:
+        if voiture in self.listedesVoitures:
             print("La voiture existe déjà")
         else:
-            if len(self.listeVoitures) < self.capacite:
-                self.listeVoitures.append(voiture)
+            if len(self.listedesVoitures) < self.capacite:
+                self.listedesVoitures.append(voiture)
                 print("Voiture ajoutée")
             else:
                 print("Parc plein")
@@ -42,5 +42,21 @@ class Parc:
 
     def calculerNbrPlacesLibres(self):
         return self.capacite - len(self.listedesVoitures)
+
+parc1 = Parc(1, "Montreal", 10)
+parc2 = Parc(2, "TORONTO", 3)
+parc3 = Parc(5, "ALGERIA", 7)
+
+
+v1 = Voiture("AA212W1", "DODGE", "Noir")
+v2 = Voiture("BC12S12", "TOYOTA", "Blanc")
+v3 = Voiture("CC333", "BMW", "Rouge")
+
+parc1.entrerVoiture(v1)
+parc2.entrerVoiture(v2)
+parc3.entrerVoiture(v3)
+parc2.sortirVoiture(v2)
+
+print("Places libres:", parc1.calculerNbrPlacesLibres())
 
 
